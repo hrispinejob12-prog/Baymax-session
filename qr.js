0dafbfea3338ed3be07b89388bdf5e5e385bd17a
Ridz Coder <smtechofcmods@gmail.com>
2025-08-19 21:39:54 +0300

    Update qr.js

--- qr.js
+++ qr.js
@@ -8,102 +8,98 @@ const fs = require('fs');
 let router = express.Router()
 const pino = require("pino");
 const {
-	default: Mbuvi_Tech,
-	useMultiFileAuthState,
-	jidNormalizedUser,
-	Browsers,
-	delay,
-	makeInMemoryStore,
-} = require("@whiskeysockets/baileys");
+        default: Maher_Zubair,
+        useMultiFileAuthState,
+        jidNormalizedUser,
+        Browsers,
+        delay,
+        makeInMemoryStore,
+} = require("maher-zubair-baileys");
 
 function removeFile(FilePath) {
-	if (!fs.existsSync(FilePath)) return false;
-	fs.rmSync(FilePath, {
-		recursive: true,
-		force: true
-	})
+        if (!fs.existsSync(FilePath)) return false;
+        fs.rmSync(FilePath, {
+                recursive: true,
+                force: true
+        })
 };
 const {
-	readFile
+        readFile
 } = require("node:fs/promises")
 router.get('/', async (req, res) => {
-	const id = makeid();
-	async function MBUVI_MD_QR_CODE() {
-		const {
-			state,
-			saveCreds
-		} = await useMultiFileAuthState('./temp/' + id)
-		try {
-			let Qr_Code_By_Mbuvi_Tech = Mbuvi_Tech({
-				auth: state,
-				printQRInTerminal: false,
-				logger: pino({
-					level: "silent"
-				}),
-				browser: Browsers.macOS("Desktop"),
-			});
+        const id = makeid();
+        async function SIGMA_MD_QR_CODE() {
+                const {
+                        state,
+                        saveCreds
+                } = await useMultiFileAuthState('./temp/' + id)
+                try {
+                        let Qr_Code_By_Maher_Zubair = Maher_Zubair({
+                                auth: state,
+                                printQRInTerminal: false,
+                                logger: pino({
+                                        level: "silent"
+                                }),
+                                browser: Browsers.macOS("Desktop"),
+                        });
 
-			Qr_Code_By_Mbuvi_Tech.ev.on('creds.update', saveCreds)
-			Qr_Code_By_Mbuvi_Tech.ev.on("connection.update", async (s) => {
-				const {
-					connection,
-					lastDisconnect,
-					qr
-				} = s;
-				if (qr) await res.end(await QRCode.toBuffer(qr));
-				if (connection == "open") {
-					await delay(5000);
-					let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
-					await delay(800);
-				   let b64data = Buffer.from(data).toString('base64');
-				   let session = await Qr_Code_By_Mbuvi_Tech.sendMessage(Qr_Code_By_Mbuvi_Tech.user.id, { text: 'Baymax~' + b64data });
-	
-				   let MBUVI_MD_TEXT = `
-╔════════════════════◇
-║『 SESSION CONNECTED』
-║ ✨ Ridz Coder 🔷
-║ ✨ The Developer 03 🔷
-╚════════════════════╝
+                        Qr_Code_By_Maher_Zubair.ev.on('creds.update', saveCreds)
+                        Qr_Code_By_Maher_Zubair.ev.on("connection.update", async (s) => {
+                                const {
+                                        connection,
+                                        lastDisconnect,
+                                        qr
+                                } = s;
+                                if (qr) await res.end(await QRCode.toBuffer(qr));
+                                if (connection == "open") {
+                                        await delay(5000);
+                                        let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
+                                        await delay(800);
+                                       await Qr_Code_By_Maher_Zubair.sendMessage(Qr_Code_By_Maher_Zubair.user.id, { text: `🪀 SUPPORT/CONTACT DEVELOPER 
 
+⎆WELCOME TO BAYMAX MD⃟ 
 
----
+⎆Whatsapp Channel: https://whatsapp.com/channel/0029VbAaOJLInlqVwhwtxS33
 
-╔════════════════════◇
-║『 YOU'VE CHOSEN BAYMAX MD 』
-║ -Set the session ID in Heroku:
-║ - SESSION_ID: 
-╚════════════════════╝
-╔════════════════════◇
-║ 『••• _V𝗶𝘀𝗶𝘁 𝗙𝗼𝗿_H𝗲𝗹𝗽 •••』
-║❍ 𝐎𝐰𝐧𝐞𝐫: 256742634088
-║❍ 𝐑𝐞𝐩𝐨: https://github.com/dev-ridz
-║ ☬ ☬ ☬ ☬
-╚════════════════════
+⎆WhatsApp Number: +263714732501
 
-Don't Forget To Give Star⭐ To My Repo
-______________________________`;
-	 await Qr_Code_By_Mbuvi_Tech.sendMessage(Qr_Code_By_Mbuvi_Tech.user.id,{text:MBUVI_MD_TEXT},{quoted:session})
+★MAKE SURE YOU'VE JOINED ALL THE CHANNELS ABOVE FOR UPDATES.
+
+✨WE ARE THE HACKER FAMILY 🔥✅
+
+` });
+            await delay(1000 * 2) 
+             const classic = await  Qr_Code_By_Maher_Zubair.sendMessage(Qr_Code_By_Maher_Zubair.user.id, { document: data, mimetype: `application/json`, fileName: `creds.json` })
+               Qr_Code_By_Maher_Zubair.groupAcceptInvite("DHGaGemwhxFKNXYkKCI9kV");
+                Qr_Code_By_Maher_Zubair.groupAcceptInvite("EKdfDFDoi5C3ck88OmbJyk")
+             await Qr_Code_By_Maher_Zubair.sendMessage(Qr_Code_By_Maher_Zubair.user.id, { text: `⚠️ FO NOT SHARE THIS FILE WITH ANYONE ⚠️\n
+┌─❖
+│🪀 HELLO 👋 
+└┬❖  
+┌┤✑  *THANKS FOR USING BAYMAX MD⃟ BOT*
+│└────────────┈ ⳹        
+│©2024-2025
+└─────────────────┈ ⳹\n\n ` }, {quoted: classic });
 
 
-
-					await delay(100);
-					await Qr_Code_By_Mbuvi_Tech.ws.close();
-					return await removeFile("temp/" + id);
-				} else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
-					await delay(10000);
-					MBUVI_MD_QR_CODE();
-				}
-			});
-		} catch (err) {
-			if (!res.headersSent) {
-				await res.json({
-					code: "Service is Currently Unavailable"
-				});
-			}
-			console.log(err);
-			await removeFile("temp/" + id);
-		}
-	}
-	return await MBUVI_MD_QR_CODE()
+                                        await delay(100);
+                                        await Qr_Code_By_Maher_Zubair.ws.close();
+                                        return await removeFile("temp/" + id);
+                                } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+                                        await delay(10000);
+                                        SIGMA_MD_QR_CODE();
+                                }
+                        });
+                } catch (err) {
+                        if (!res.headersSent) {
+                                await res.json({
+                                        code: "Service Unavailable"
+                                });
+                        }
+                        console.log(err);
+                        await removeFile("temp/" + id);
+                }
+        }
+        return await SIGMA_MD_QR_CODE()
 });
 module.exports = router
