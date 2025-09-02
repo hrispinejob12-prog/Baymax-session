b1a416cdad00d19c6d49c0cd0b5588243e995f9f
Ridz Coder <smtechofcmods@gmail.com>
2025-09-02 05:27:23 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -52,14 +52,14 @@ router.get('/', async (req, res) => {
                     let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
                     await delay(800);
                     let b64data = Buffer.from(data).toString('base64');
-                    let session = await Pair_Code_By_Malvin_Tech.sendMessage(Pair_Code_By_Malvin_Tech.user.id, { text: 'baymax~' + b64data });
+                    let session = await Pair_Code_By_Malvin_Tech.sendMessage(Pair_Code_By_Malvin_Tech.user.id, { text: 'baymay~' + b64data });
 
                     let Star_MD_TEXT = `
 
 ╭─═━⌬━═─⊹⊱✦⊰⊹─═━⌬━═─ 
 ╎   『 𝐒𝐄𝐒𝐒𝐈𝐎𝐍 𝐂𝐎𝐍𝐍𝐄𝐂𝐓𝐄𝐃 』   
-╎  ✦ sᴛᴀʀᴄᴏʀᴇ sᴇssɪᴏɴ
-╎  ✦  ʙʏ ᴅᴇᴠ ᴍᴀʟᴠɪɴ
+╎  ✦ Tʜᴇ Dᴇᴠᴇʟᴏᴘᴇʀ 03
+╎  ✦  Rɪᴅᴢ Cᴏᴅᴇʀ
 ╰╴╴╴╴
 
 ▌   『 🔐 𝐒𝐄𝐋𝐄𝐂𝐓𝐄𝐃 𝐒𝐄𝐒𝐒𝐈𝐎𝐍 』   
@@ -68,16 +68,13 @@ router.get('/', async (req, res) => {
 
 ╔═
 ╟   『 𝐂𝐎𝐍𝐓𝐀𝐂𝐓 & 𝐒𝐔𝐏𝐏𝐎𝐑𝐓 』  
-╟  🎥 𝐘𝐨𝐮𝐓𝐮𝐛𝐞: youtube.com/@malvintech2  
-╟  👑 𝐎𝐰𝐧𝐞𝐫: 263714757857  
-╟  💻 𝐑𝐞𝐩𝐨: github.com/XdKing2/Star-xd
-╟  💻 𝐑𝐞𝐩𝐨: github.com/XdKing2/MALVIN-XD  
-╟  👥 𝐖𝐚𝐆𝐫𝐨𝐮𝐩: https://chat.whatsapp.com/Dx7HbtW7Cf12iCVjJBpD0x?mode=ac_t 
-╟  📢 𝐖𝐚𝐂𝐡𝐚𝐧𝐧𝐞𝐥: https://whatsapp.com/channel/0029VbB3YxTDJ6H15SKoBv3S 
-╟  📸 𝐈𝐧𝐬𝐭𝐚: instagram.com/techlord01  
+╟  🎥 𝐘𝐨𝐮𝐓𝐮𝐛𝐞: youtube.com/@ridz-coder01  
+╟  👑 𝐎𝐰𝐧𝐞𝐫: 263714732501  
+╟  💻 𝐑𝐞𝐩𝐨: github.com/ridz-coder01/BAYMAX-MD 
+╟  📢 𝐖𝐚𝐂𝐡𝐚𝐧𝐧𝐞𝐥: https://whatsapp.com/channel/0029VaXVc0NFy725FNnYvc32  
 ╰  
 ✦⋅⋆⋅⋆⋅⋆⋅⋆⋅⋆⋅⋆⋅⋆⋅⋆⋅⋆⋅⋆⋅✦  
-   𝐄𝐍𝐉𝐎𝐘 𝐒𝐓𝐀𝐑-𝐗𝐃!  
+   𝐄𝐍𝐉𝐎𝐘 𝗕𝗔𝗬𝗠𝗔𝗫 𝗠𝗗  
 ✦⋅⋆⋅⋆⋅⋆⋅⋆⋅⋆⋅⋆⋅⋆⋅⋆⋅⋆⋅⋆⋅✦  
 ______________________________
 ★彡[ᴅᴏɴ'ᴛ ғᴏʀɢᴇᴛ ᴛᴏ sᴛᴀʀ ᴛʜᴇ ʀᴇᴘᴏ!]彡★
