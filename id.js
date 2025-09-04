b051cf222a805c94460a3330ae6e59e8e9f9e2ee
hrispinejob12-prog <hrispinejob12@gmail.com>
2025-09-03 17:34:24 -0700

    Update id.js

--- id.js
+++ id.js
@@ -1,3 +1,4 @@
+// id.js
 function makeid(num = 4) {
   let result = "";
   let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -7,4 +8,14 @@ function makeid(num = 4) {
   }
   return result;
 }
-module.exports = {makeid};
+
+/**
+ * Generates a unique session name with a prefix.
+ * @returns {string} A unique session name (e.g., baymax_a1b2c).
+ */
+function generateSessionName() {
+    const randomPart = makeid(5).toLowerCase();
+    return `baymax_${randomPart}`;
+}
+
+module.exports = { makeid, generateSessionName };
