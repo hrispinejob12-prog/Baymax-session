f2a36e51dc7758de72143f92ddd9df4c32f4d73a
hrispinejob12-prog <hrispinejob12@gmail.com>
2025-09-03 17:33:15 -0700

    Update Ridz.js

--- Ridz.js
+++ Ridz.js
@@ -1,26 +1,40 @@
+// Ridz.js
 const express = require('express');
+const path = require('path'); // Make sure path module is imported
 const app = express();
-__path = process.cwd()
+__path = process.cwd();
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
 let server = require('./qr'),
     code = require('./pair');
+
 require('events').EventEmitter.defaultMaxListeners = 500;
+
+// --- Static Route to Serve Session Files ---
+// This makes the files in your 'sessions' directory available to be fetched by your bot.
+app.use('/sessions', express.static(path.join(__dirname, 'sessions')));
+
+// --- API Routes ---
 app.use('/qr', server);
 app.use('/code', code);
-app.use('/pair',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
-})
-app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/main.html')
-})
+
+// --- HTML Page Routes ---
+app.use('/pair', async (req, res, next) => {
+    res.sendFile(__path + '/pair.html');
+});
+
+app.use('/', async (req, res, next) => {
+    res.sendFile(__path + '/main.html');
+});
+
+// --- Middleware & Server Start ---
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(PORT, () => {
     console.log(`
-Don't Forget To Give Star
+Don't Forget To Give Star ⭐
 
- Server running on http://localhost:` + PORT)
-})
+Server running on http://localhost:${PORT}`);
+});
 
-module.exports = app
+module.exports = app;
